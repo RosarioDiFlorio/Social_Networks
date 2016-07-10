a7dcaf859282008150a709878fed732163692946
RosarioUbuntu <r.diflorio93@gmail.com>
2016-07-10 17:35:13 +0200

    preparazione del tester dei matching

--- social_networks/Script/Test_Dataset_Matching.py
+++ social_networks/Script/Test_Dataset_Matching.py
@@ -1,20 +1,66 @@
 #!/usr/bin/python
 from load_dataset import *
 from Dataset_Matching import *
+import timeit
+
+
+def set_default(obj):
+	if isinstance(obj,set):
+		return list(obj)
+	raise TypeError
 
 db_normal = db()
 total_len_docs = len(db_normal.keys())
-print total_len_docs
+
+
 inverted_db = ordered_reverse_db()
-query = "game differenziata"
-print("QUERY: "+query)
 
-result = best_match_opt(query, 0.02, inverted_db,total_len_docs)
-print("BEST MATCH: "+str(result))
-print "len: " + str(len(result))
+nquery = 0
+elapsed = 0
+elapsed_opt = 0
+niter = 1000
+
+result_dict = dict()
+result_opt_dict = dict()
+result = list()
+result_opt = list()
+
+with open("query.txt") as file:
+    print("---------STARTING ---------")
+    while True:
+        query = file.readline()
+        #print (" Num Query -> "+ str(nquery) + " Iterations -> " + str(niter))
+        if query == "": break
+        for i in range(niter):
+            
+            start_time = timeit.default_timer()
+            result = best_match(query, 0.02, db_normal)
+            elapsed += timeit.default_timer() - start_time
+            
+            start_time = timeit.default_timer()
+            result_opt = best_match_opt(query, 0.02, inverted_db,total_len_docs)
+            elapsed_opt += timeit.default_timer() - start_time
+        result_dict[query] = result
+        result_opt_dict[query] = result_opt
+        nquery += 1
+        
+       
+    print("---------FINISHED---------")
+    print("---------DUMP RESULT DOCUMENTS-------------")
+    with open('result_match.json', 'w') as fp:
+            stri = json.dumps(result_dict, ensure_ascii=False, encoding="utf-8", default = set_default)
+            fp.write(stri)
+    with open('result_opt_match.json', 'w') as fp:
+            stri = json.dumps(result_opt_dict, ensure_ascii=False, encoding="utf-8", default = set_default)
+            fp.write(stri)
+    print("---------END DUMP-------------")
+    print("---------TIMES------------")
+    print ("Num Iterations -> "+ str(niter))
+    print ("Num Query -> " +str(nquery))
+    print ("Time best_match -> "+ str(float(elapsed/nquery)))
+    print ("Time best_match_opt -> "+ str(float(elapsed_opt/nquery)))
+    print("--------------------------")
 
 
-'''
-result2 = improved_best_match(query, inverted_db)
-print("IMPROVED BEST MATCH: "+str(result2))
-'''
+
+
