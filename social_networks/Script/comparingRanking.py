436cb1fd820c64f10aed87b0601f0ef3ab532edf
Delkatis <v.venosi@gmail.com>
2016-07-11 11:49:07 +0200

    comparingRanking

--- social_networks/Script/comparingRanking.py
+++ social_networks/Script/comparingRanking.py
@@ -1,6 +1,8 @@
 from load_dataset import * 
 from collections import OrderedDict
 import operator
+import xlwt
+import random
 
 PageRank = get_PageRank_graph()
 HITS = get_HITS_graph()
@@ -27,11 +29,54 @@ HITS_H_key = list(HITS_H.keys())
 HITS_tot_key = list(HITS_tot.keys())
 
 
-for i,k in enumerate(PageRank_key):
+#code for write in a xlsx file
+filename = "risultati_Rank2.xlsx"
+sheet = "result_ranking"
 
-	j_a = HITS_A_key.index(k)
-	j_h = HITS_H_key.index(k)
+book = xlwt.Workbook(encoding="utf-8")
+sheet1 = book.add_sheet(sheet)
+
+vertex = 0
+colPagerankValue = 1
+colHitsTOTValue = 2
+
+sheet1.write(0,vertex, "vertex")
+sheet1.write(0,colPagerankValue, "PageRank Value")
+sheet1.write(0,colHitsTOTValue, "Hits TOT Value")
+
+valMax = len(PageRank)
+valtmp = valMax
+#print(valMax)
+
+pr = dict()
+hits = dict()
+
+for i,k in enumerate(PageRank_key):
+        
 	j_t = HITS_tot_key.index(k)
-	print(i,j_a)
-	print(i,j_h)
-	print(i,j_t)
+        pr[i] = float(valtmp)/valMax
+	hits[j_t] = float(valtmp)/valMax
+	
+	valtmp -= 1
+        
+
+print("shuffle dictionaries")
+#shuffle dict for write shuffle nodes in xlsx file
+
+arr_shuffle = dict()
+for i in range(len(PageRank)):
+    arr_shuffle[i] = i
+    
+random.shuffle(arr_shuffle)
+ 
+row = 2
+print("print in file")
+for i in arr_shuffle:
+        
+        sheet1.write(row, vertex, str(arr_shuffle[i]))
+        sheet1.write(row, colPagerankValue,pr[arr_shuffle[i]])
+        sheet1.write(row, colHitsTOTValue, hits[arr_shuffle[i]])
+    
+        row += 1
+        
+book.save(filename)
